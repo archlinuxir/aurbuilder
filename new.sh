0fbd27ab859f2b9b71ba9703787638ad3bc83039
Bardia Moshiri <fakeshell@bardia.tech>
2022-02-14 23:16:22 +0330

    added new packages script

---
+++ new.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+# Sleeps exist to prevent a set of bugs from happening.
+# I Use proxychains because without it downloading will be slow
+
+cd /home/bardia
+BUILDNEW=$(date +'%Y-%m-%d-%H-%M-%S')
+echo "Updating the system."
+sudo pacman -Syu --noconfirm >> /dev/null
+
+# This loop reads the file that countains our packages in my case its 'pkg2'.
+
+while IFS= read -r line; do
+     proxychains archlinuxir_dep.sh $line | tee -a /home/bardia/logs/build/build-$BUILDNEW
+done < /home/bardia/pkg2
+
+sleep 1s
+rm /home/bardia/source -rf && cd /home/bardia
